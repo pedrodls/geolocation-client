33485a87b10b096a1330413e64fc4751c64a5c30
pedrodls <pedrojoaodev@gmail.com>
2022-01-25 17:56:59 +0100

    App module and component fixed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { CommonModule, HashLocationStrategy, LocationStrategy } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { NavbarComponent } from 'src/components/shared/navbar/navbar.component';
@@ -5,19 +6,25 @@ import { NavbarComponent } from 'src/components/shared/navbar/navbar.component';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app.routing';
 import { LandingComponent } from './landing/landing.component';
+import { LeafletMapComponent } from './leaflet-map/leaflet-map.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     LandingComponent,
+    LeafletMapComponent,
     NavbarComponent
   ],
   imports: [
     BrowserModule,
+    CommonModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    {provide: LocationStrategy, useClass: HashLocationStrategy}
+
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
