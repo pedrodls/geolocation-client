983c66b7c76391590e2585365dee2b2eb469ceeb
pedrodls <pedrojoaodev@gmail.com>
2022-01-25 17:57:20 +0100

    app routing fixed

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingComponent } from './landing/landing.component';
+import { LeafletMapComponent } from './leaflet-map/leaflet-map.component';
 
 const routes: Routes = [
+  {
+    path: '', redirectTo: 'landing', pathMatch: 'full'
+  },
+  {
+    path: 'landing',
+    component: LandingComponent,
+  },
+  {
+    path: 'mapping',
+    component: LeafletMapComponent,
+  }
 ];
 
 @NgModule({
